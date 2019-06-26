2de27ed5f202b6923c06b26af35221e4064c1e42
Your Name <you@example.com>
2019-06-26 20:40:52 +0700

    add file

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::prefix('customer')->group(function () {
+    Route::get('index', function () {
+        return view('index');
+    });
+
+    Route::get('create', 'HomeController@create')->name('create');
+
+    Route::post('add', 'HomeController@add')->name('add');
+
+    Route::get('show/{id}', 'HomeController@show')->name('show');
+
+    Route::get('delete/{id}', 'HomeController@delete')->name('delete');
+
+    Route::post('store', function () {
+        // Xử lý lưu dữ liệu tạo khách hàng thong qua phương thức POST từ form
+    });
+
+    Route::get('{id}/show', function () {
+        // Hiển thị thông tin chi tiết khách hàng có mã định danh id
+    });
+
+    Route::get('{id}/edit', function () {
+        // Hiển thị Form chỉnh sửa thông tin khách hàng
+    });
+
+    Route::patch('{id}/update', function () {
+        // xử lý lưu dữ liệu thông tin khách hàng được chỉnh sửa thông qua PATCH từ form
+    });
+
+    Route::delete('{id}', function () {
+        // Xóa thông tin dữ liệu khách hàng
+    });
+});
